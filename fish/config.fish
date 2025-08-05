47e16b40579eb903ed0d8010e0844e7cd9c8b229
shyuuun <m4rshm4ll0w14@gmail.com>
2025-08-06 00:31:46 +0800

    removed the nvim config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,15 @@
 if status is-interactive
-    neofetch
+    fastfetch
 end
 
 set -gx PATH $PATH /usr/bin/flutter
 
 # disable fish greeting
 set -U fish_greeting
+
+# Aliases
+alias gc="git commit"
+alias gch="git checkout"
+alias gs="git status"
+alias gpl="git pull"
+alias gph="git push"
